c8c7c630fe2bf90e8bc8fc4202ff4c64e6e97eec
wtigga <wtigga@gmail.com>
2018-11-05 22:10:49 +0800

    Working prototype

--- main.py
+++ main.py
@@ -1,9 +1,61 @@
 # coding: utf-8
 import csv
+import datetime
+import pytz
+
+#  receive in YYYY-MM-DD hh:mm format, return as a dict in the target time zone
+def convert_date_time(date_source, timezone_source, timezone_target):
+    tz1 = pytz.timezone(timezone_source)  # initial time zone
+    tz2 = pytz.timezone(timezone_target)  # target timezone
+    date_source = datetime.datetime.strptime(date_source, '%Y-%m-%d %H:%M')
+    date_source = tz1.localize(date_source)
+    date_source = date_source.astimezone(tz2)
+    year = date_source.strftime('%Y')
+    month = date_source.strftime('%m')
+    day = date_source.strftime('%d')
+    hour = date_source.strftime('%H')
+    minute = date_source.strftime('%M')
+    date_time_list = {'year': year, 'month': month, 'day': day, 'hour': hour, 'minute': minute}
+    return date_time_list
+
+#  receive date in YYYY-MM-DD hh:mm format, return in the same format
+def user_date(date_plain):
+    date_plain = date_plain.rstrip()
+    date_plain = date_plain.lstrip()
+    year = date_plain[0:4]
+    if int(year) > 2049 or int(year) < 2000:
+        print('Error: Years should be between 2000 and 2049')
+        return
+    else:
+        pass
+    month = date_plain[5:7]
+    if int(month) > 12 or int(month) < 1:
+        print('Error: Months are between 1 and 12')
+        return
+    else:
+        pass
+    day = date_plain[8:10]
+    if int(day) > 31 or int(day) < 1:
+        print('Error: Days are between 1 and 30')
+        return
+    else:
+        pass
+    hour = date_plain[11:13]
+    if int(hour) > 23 or int(hour) < 0:
+        print('Error: Hours are between 0 and 23')
+        return
+    else:
+        pass
+    minute = date_plain[14:16]
+    if int(minute) > 59 or int(day) < 0:
+        print('Error: Minutes are betwee 0 and 59')
+        return
+    else:
+        pass
+    date_source = (year + '-' + month + '-' + day + ' ' + hour + ':' + minute)
+    return date_source
 
 #  function to read required info from CSV files with language related data
-
-
 def read_template(filename):
     csv_delim = '\t'
     # getting the first cell content and using it as ID column
@@ -24,7 +76,8 @@ def read_template(filename):
     return sorted_templates
 
 
-months = read_template('./localized/months_type1.csv')
+months_1 = read_template('./localized/months_type1.csv')
+months_2 = read_template('./localized/months_type2.csv')
 templates = read_template('./localized/templates.csv')
 
 
@@ -34,14 +87,14 @@ def date_text_output(time_from_user_1, time_from_user_2, template_number, langua
     # take month num from time_from_user, find same num in months dict, find the right language by the language key
 
     time_formatted = {
-        'MM_1_type1': (months[time_from_user_1['month']])[language],
-        'MM_1_type2': (months[time_from_user_1['month']])[language],
+        'MM_1_type1': (months_1[time_from_user_1['month']])[language],
+        'MM_1_type2': (months_2[time_from_user_1['month']])[language],
         'DD_1': time_from_user_1['day'],
         'YYYY_1': time_from_user_1['year'],
         'hh_1': time_from_user_1['hour'],
         'mm_1': time_from_user_1['minute'],
-        'MM_2_type1': (months[time_from_user_2['month']])[language],
-        'MM_2_type2': (months[time_from_user_2['month']])[language],
+        'MM_2_type1': (months_1[time_from_user_2['month']])[language],
+        'MM_2_type2': (months_2[time_from_user_2['month']])[language],
         'DD_2': time_from_user_2['day'],
         'YYYY_2': time_from_user_2['year'],
         'hh_2': time_from_user_2['hour'],
@@ -51,10 +104,21 @@ def date_text_output(time_from_user_1, time_from_user_2, template_number, langua
     return output
 
 
-temp_num = '10'
-time_1 = {'year': '2017', 'month': '01', 'day': '30', 'hour': '13', 'minute': '00'}
-time_2 = {'year': '2018', 'month': '02', 'day': '15', 'hour': '10', 'minute': '59'}
-lang = 'RU'
+timezone_source_default = 'Asia/Shanghai'
+timezone_target_default = 'Europe/Moscow'
+temp_num = '12'
+
+# time = user_date(input('First date as YYYY-MM-DD hh:mm: '))
+time = '2018-10-25 16:00'
+time_1 = convert_date_time(time, timezone_source_default, timezone_target_default)
+
+# time = user_date(input('Second date as YYYY-MM-DD hh:mm: '))
+time = '2018-11-11 16:00'
+time_2 = convert_date_time(time, timezone_source_default, timezone_target_default)
+
+# lang = input('Input language')
 
 
-print(date_text_output(time_1, time_2, temp_num, lang))
+# print(date_text_output(time_1, time_2, temp_num, lang))
+print(date_text_output(time_1, time_2, temp_num, 'EN'))
+
